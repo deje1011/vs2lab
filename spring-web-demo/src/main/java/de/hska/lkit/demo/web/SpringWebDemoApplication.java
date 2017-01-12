0df0f4ceb2a7d4ff939e50cc9b46887deb1e84fe
Marina <marinam3733@gmail.com>
2017-01-12 14:58:45 +0100

    Change UserX and change host ip

--- spring-web-demo/src/main/java/de/hska/lkit/demo/web/SpringWebDemoApplication.java
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/SpringWebDemoApplication.java
@@ -3,10 +3,13 @@ package de.hska.lkit.demo.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringWebDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebDemoApplication.class, args);
+
+
 	}
 }
