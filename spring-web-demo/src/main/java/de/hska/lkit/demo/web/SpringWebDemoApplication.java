ce6b2321febd8422a865c5db7d209f7285d57c66
deje1011 <jessedesaever@Jesses-MacBook-Pro.local>
2017-01-13 12:03:57 +0100

    :sparkles: Make sockets work with multiple tomcat instances

--- spring-web-demo/src/main/java/de/hska/lkit/demo/web/SpringWebDemoApplication.java
+++ spring-web-demo/src/main/java/de/hska/lkit/demo/web/SpringWebDemoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringWebDemoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebDemoApplication.class, args);
+        SpringApplication.run(SpringWebDemoApplication.class, args);
 	}
 }
